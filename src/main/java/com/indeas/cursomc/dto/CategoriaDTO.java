ade48d6de1b9a1f151a009e532f8405bfd004cf7
Alexsandro Lima <alexlima_cti@hotmail.com>
2018-12-31 13:50:30 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/indeas/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/indeas/cursomc/dto/CategoriaDTO.java
@@ -2,8 +2,9 @@ package com.indeas.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import com.indeas.cursomc.domain.Categoria;
 
