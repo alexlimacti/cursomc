e32d223b97a2315c0cf89875cbbd88ccc3cacef0
Alexsandro Lima <alexlima_cti@hotmail.com>
2019-01-03 13:45:39 -0300

    Validacao customizada nao repetido na insercao de cliente!

--- src/main/java/com/indeas/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/indeas/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.indeas.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.indeas.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	public Cliente findByEmail(String email);
 
 }
--- src/main/java/com/indeas/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/indeas/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.indeas.cursomc.domain.Cliente;
 import com.indeas.cursomc.domain.enuns.TipoCliente;
 import com.indeas.cursomc.dto.ClienteNewDTO;
+import com.indeas.cursomc.repositories.ClienteRepository;
 import com.indeas.cursomc.resources.exception.FieldMessage;
 import com.indeas.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ Inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email","Email já existente"));
+		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
