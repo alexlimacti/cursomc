0e932c37a4fa8c2be287e7a08cc6965863c00241
Alexsandro Lima <alexlima_cti@hotmail.com>
2019-01-12 07:50:42 -0300

    Implementando toString de Pedido

--- src/main/java/com/indeas/cursomc/services/PedidoService.java
+++ src/main/java/com/indeas/cursomc/services/PedidoService.java
@@ -13,7 +13,6 @@ import com.indeas.cursomc.domain.enuns.EstadoPagamento;
 import com.indeas.cursomc.repositories.ItemPedidoRepository;
 import com.indeas.cursomc.repositories.PagamentoRepository;
 import com.indeas.cursomc.repositories.PedidoRepository;
-import com.indeas.cursomc.repositories.ProdutoRepository;
 import com.indeas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,7 +28,10 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
-	private ProdutoRepository produtoRepository;
+	private ProdutoService produtoService;
+
+	@Autowired
+	private ClienteService clienteService;
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
@@ -43,6 +45,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -53,10 +56,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for(ItemPedido ip: obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoRepository.findById(ip.getProduto().getId()).get().getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
